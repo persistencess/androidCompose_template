cef21f0a84dc009741f98ea68349992aadb3c733
xczcdjx <10097251+xczcdjx@user.noreply.gitee.com>
2024-12-13 17:19:37 +0800

    时间方法调整

--- app/src/main/java/com/xczcdjx/word/utils/dateFormat.kt
+++ app/src/main/java/com/xczcdjx/word/utils/dateFormat.kt
@@ -10,28 +10,31 @@ import kotlinx.datetime.format.char
 import kotlinx.datetime.toInstant
 import kotlinx.datetime.toLocalDateTime
 
-fun formatDate(isoDateString: String): String {
-    // 解析ISO 8601日期字符串为Instant对象
-    val instant = Instant.parse(isoDateString)
-    // 将Instant对象转换为本地日期时间对象
-    val localDateTime = instant.toLocalDateTime(TimeZone.currentSystemDefault())
-    // 提取日期部分并格式化为标准日期字符串
-    return localDateTime.date.toString()
-}
-
-fun formatDateTime(isoDateString: String): String {
+// UTC 转标准时间
+fun formatDate(
+    isoDateString: String,
+    showTime: Boolean = false,
+    zone: TimeZone = TimeZone.currentSystemDefault()
+): String {
     val customFormat = LocalDateTime.Format {
         date(LocalDate.Formats.ISO)
-        char(' ')
-        hour(); char(':'); minute(); char(':'); second()
+        if (showTime) {
+            char(' ')
+            hour(); char(':'); minute(); char(':'); second()
 //        char(','); secondFraction(fixedLength = 3)
+        }
     }
     val instant = Instant.parse(isoDateString)
-    val localDateTime = instant.toLocalDateTime(TimeZone.currentSystemDefault())
+    val localDateTime = instant.toLocalDateTime(zone)
     return localDateTime.format(customFormat)
 }
 
-fun formatNow(now: Instant = Clock.System.now(), showTime: Boolean = false): String {
+// Instant 转 标准时间
+fun formatNow(
+    now: Instant = Clock.System.now(),
+    showTime: Boolean = false,
+    zone: TimeZone = TimeZone.currentSystemDefault()
+): String {
     val cF = LocalDateTime.Format {
         date(LocalDate.Formats.ISO)
         if (showTime) {
@@ -40,16 +43,26 @@ fun formatNow(now: Instant = Clock.System.now(), showTime: Boolean = false): Str
 //        char(','); secondFraction(fixedLength = 3)
         }
     }
-    val localDT = now.toLocalDateTime(TimeZone.currentSystemDefault())
+    val localDT = now.toLocalDateTime(zone)
     return localDT.format(cF)
 }
 
-fun convertStrToMill(str: String):Long {
+// 字符串转时间戳
+fun convertStrToMill(str: String, zone: TimeZone = TimeZone.currentSystemDefault()): Long {
     val format = LocalDateTime.Format {
         date(LocalDate.Formats.ISO)
         char(' ')
         hour(); char(':'); minute(); char(':'); second()
     }
-    return LocalDateTime.parse(str, format).toInstant(TimeZone.currentSystemDefault())
+    return LocalDateTime.parse(str, format).toInstant(zone)
         .toEpochMilliseconds()
+}
+
+// 时间戳转回标准时间
+fun convertMillToDate(
+    m: Long,
+    showTime: Boolean = false,
+    zone: TimeZone = TimeZone.currentSystemDefault()
+): String {
+    return formatNow(Instant.fromEpochMilliseconds(m), showTime, zone)
 }
